f1ab77353755b3bd1ddacdbb3ab50737e782f9a1
Vithuyan Sivananthan <vithuyan.s@outlook.com>
2019-02-22 14:51:15 -0500

    done

---
+++ paperboy.py
@@ -0,0 +1,16 @@
+class Paperboy:
+ def __init__(self, name, experience =0, earnings =0):
+     self.name = name
+     self.experience = experience
+     self.earnings = earnings
+
+def __str__(self):
+    return ("{} has delivered {} papers and he has made {}.". format(self.name, self.experience, self.earnings)
+
+def quota(self):
+    return 50 + (self.experience / 2)
+
+def deliver(self, start_address, end_address):
+    num_of_houses = (end_address + 1) - start_address
+    house = 1
+    current_quota = self.paper()            
